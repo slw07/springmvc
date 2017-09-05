cfcb9c139074bfb6f155ea9731d630fe98cb860c
songliwei <slw07@sina.com>
2017-09-05 16:50:18 +0800

    集成mybatis，调试阶段

--- hadoop/src/main/java/com/slw/spring/mr/controller/LoginController.java
+++ hadoop/src/main/java/com/slw/spring/mr/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.slw.hadoop.mr.controller;
+package com.slw.spring.mr.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
